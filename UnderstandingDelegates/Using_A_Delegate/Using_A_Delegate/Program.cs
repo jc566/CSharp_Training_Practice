3cb2854a449084888df46e5aa083809deb3c3fa3
jc566 <jc566@njit.edu>
2017-09-21 11:40:58 -0400

    add comments to using a delefate

--- UnderstandingDelegates/Using_A_Delegate/Using_A_Delegate/Program.cs
+++ UnderstandingDelegates/Using_A_Delegate/Using_A_Delegate/Program.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 
+/*
+ * In C# Delegates form the basic building blocks for events.
+ * A DELEGATE is a type that defines a method signature.
+ * In C++ for example, you would do this with a function pointer.
+ * In C# you can instantiate a delegate and let it point to another method.
+ * You can invoke the method through DELEGATE.
+ * */
 namespace Using_A_Delegate
 {
     class Program
@@ -23,3 +30,7 @@ namespace Using_A_Delegate
         }
     }
 }
+/*
+ * As you can see, you use the special DELEGATE keyword to tell the compiler tha you are creating a delegate type.
+ * DELEGATES can be nested in other types and they can then be used as a nested type.
+ * */
