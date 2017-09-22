3940c343d8854d329df72668df97af7c23d36dd6
jc566 <jc566@njit.edu>
2017-09-22 14:00:11 -0400

    added chapter 3 stuff

---
+++ Chapter_1/1.5/ThrowingExceptions/Throwing_An_ArgumentNullException/Throwing_An_ArgumentNullException/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Throwing_An_ArgumentNullException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static string OpenAndParse(string filename)
+        {
+            if (string.IsNullOrWhiteSpace(filename))
+            {
+                throw new ArgumentNullException("filename", "Filename is required");
+            }
+
+            /*
+             * NOT FINISHED
+             * */
+            
+        }
+    }
+}
---
+++ Chapter_3/UsingRegularExpressions/Collapse_Multiple_Spaces_RegEx/Collapse_Multiple_Spaces_RegEx/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Collapse_Multiple_Spaces_RegEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexOptions options = RegexOptions.None;
+            Regex regex = new Regex(@"[ ]{2,}", options); //theres a space between the [ and ]
+
+            string input = "1 2 3 4     5";
+            string result = regex.Replace(input, " ");
+
+            Console.WriteLine(result);
+        }
+    }
+}
+/*
+ * Although regex looks more difficult than writing the validation code in plain C#,
+ * its definitely worth learning how it works.
+ * A regular expression can dramatically simplify your code and its worth examining if you are in a situation requiring validation.
+ * */
